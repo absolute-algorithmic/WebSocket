85481727149b83a547ef67ecf80cbdb8feecb17c
KannaAny <kanna.any@proton.me>
2024-07-22 20:21:33 -0700

    Message string

--- src/websocket.ts
+++ src/websocket.ts
@@ -5,14 +5,23 @@ const PORT = process.env.PORT || 8080;
 const wss = new WebSocket.Server({ port: Number(PORT) });
 
 wss.on("connection", (ws: WebSocket) => {
-  console.log("New connection");
+  console.log("New connection established");
 
   ws.on("message", (message: WebSocket.Data) => {
-    console.log("Message received: ", message);
+    const messageString = Buffer.isBuffer(message)
+      ? message.toString()
+      : message;
+    console.log("Message received: ", messageString);
+    ws.send("Hello!");
   });
 
-  ws.on("close", () => {
-    console.log("Connection closed");
+  // https://datatracker.ietf.org/doc/html/rfc6455
+  ws.on("close", (code: number, reason: Buffer) => {
+    console.log(
+      "Connection closed with code: ",
+      code + " and reason: ",
+      reason.toString()
+    );
   });
 
   ws.on("error", (error: Error) => {
