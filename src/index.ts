756669c08f3fff99eca948a10342988be2119170
KannaAny <kanna.any@proton.me>
2024-07-22 20:10:29 -0700

    clean

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
-import express from 'express';
+import "./websocket";
 
-const app = express();
-const port = 3000;
-
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+console.log("Server running");
